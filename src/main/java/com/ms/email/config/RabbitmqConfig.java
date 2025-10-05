cd850b872eb5274833b5914baaa4c6487494fe77
Jorge Campos de Jesus <jorgecamposdevs@gmail.com>
2025-10-05 14:27:06 -0300

    first commit

---
+++ src/main/java/com/ms/email/config/RabbitmqConfig.java
@@ -0,0 +1,29 @@
+package com.ms.email.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    @Value("${broker.queue.email.name}")
+    private String queue;
+
+    @Value("${spring.rabbitmq.address}")
+    private String address;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public CachingConnectionFactory rabbitConnectionFactory() {
+        var cf = new CachingConnectionFactory();
+        cf.setUri(address);
+        return cf;
+    }
+}
